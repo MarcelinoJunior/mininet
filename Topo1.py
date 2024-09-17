ef3da976d53def19b5b7f9bce58e981857204af3
MarcelinoJunior <marcelino.dias.junior@gmail.com>
2024-09-17 00:27:54 -0300

    Update Topo1.py

--- Topo1.py
+++ Topo1.py
@@ -39,5 +39,5 @@ def simpleNetwork():
     net.stop()
 
 if __name__ == '__main__':
-    setLogLevel('info')  # Defina o nível de log para 'info' para ver as mensagens de log
+    setLogLevel('info')  # Defina o nível de log para 'info' para ver as mensagens de log.
     simpleNetwork()
